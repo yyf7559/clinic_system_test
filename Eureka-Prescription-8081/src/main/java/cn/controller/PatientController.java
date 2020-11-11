7a81a30eff748f7b7159d5bf99be6f4df10f197a
smy <151625667@qq.com>
2020-11-11 17:50:20 +0800

    gitTest4

--- Eureka-Prescription-8081/src/main/java/cn/controller/PatientController.java
+++ Eureka-Prescription-8081/src/main/java/cn/controller/PatientController.java
@@ -34,5 +34,6 @@ public class PatientController {
         System.out.println("test1");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
     }
 }
